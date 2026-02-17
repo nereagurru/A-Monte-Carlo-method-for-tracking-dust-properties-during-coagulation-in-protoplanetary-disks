a8c163b2b11b9dac23333b7a18bc3cb26c7d26d4
Nerea Gurrutxaga <78503555+nereagurru@users.noreply.github.com>
2026-02-17 15:33:40 +0100

    Add files via upload

---
+++ src_mcdust/constants.F90
@@ -0,0 +1,19 @@
+! module gathers physical constants values used in the simulations
+module constants
+   implicit none
+   public
+   real, parameter        :: pi      = 3.14159265358979323846
+   real, parameter        :: kB      = 1.38065e-16               ! Boltzmann constant [erg/K]
+   real, parameter        :: third   = 1./3.                     ! http://xkcd.com/1275/
+   real, parameter        :: AU      = 1.49597870691e+13         ! AU in cm 
+   real, parameter        :: year    = 365.256363051*24.0*3600.0 ! year in sek
+   real, parameter        :: NAv     = 6.02214076e+23            ! Avogadro constant
+   real, parameter        :: muH2     = 2.34                ! mean molecular weight of gas
+   real, parameter        :: muH2O    = 18.                 ! mean molecular weight of water molecule
+   real, parameter        :: mH2     = muH2 / NAv                 ! mean particle mass of gas
+   real, parameter        :: mH2O    = muH2O / NAv                  ! mean particle mass of water molecule
+   real, parameter        :: Ggrav   = 6.67430e-8                ! gravitational constant
+   real, parameter        :: Msun    = 1.988416e+33                ! mass of the Sun (used as mass of central star)
+   real, parameter        :: AH2     = 2.e-15                    ! geometrical cross-section of H2 molecule
+   real, parameter        :: smallv  = 1.e-10                    ! ridiculously small value
+end
