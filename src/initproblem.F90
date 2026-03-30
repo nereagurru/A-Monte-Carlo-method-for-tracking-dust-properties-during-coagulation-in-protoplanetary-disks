664fe6c2105c477cee36ee939499cde615f0f627
Nerea Gurrutxaga <78503555+nereagurru@users.noreply.github.com>
2026-03-30 14:10:05 +0200

    Update initproblem.F90
    after 1st referee report

--- src/initproblem.F90
+++ src/initproblem.F90
@@ -3,9 +3,10 @@ module initproblem
 
    use constants,  only: pi, third, AU
    use discstruct, only: gasmass, cs, omegaK
-   use parameters, only: dtg, minrad0, maxrad0, r0
+   use parameters, only: dtg, minrad0, maxrad0, r0, alpha_t, vfrag
 #ifdef MULTI_COMPONENT
    use parameters, only: matdenssi, matdensw
+   use discstruct, only: sigmag
 #else
    use parameters, only: matdens
 #endif
@@ -24,18 +25,38 @@ module initproblem
    contains
    
    ! initializing the swrm array and some variables
-   subroutine init_swarms(Ntot, swrm)
+   subroutine init_swarms(Ntot, swrm &
+#ifdef MULTI_COMPONENT
+                        &, vol_0D &
+#endif
+                        &)
       implicit none
       type(swarm), dimension(:), allocatable, target   :: swrm        ! list of all swarms in the simulation
       integer, intent(in)                              :: Ntot        ! number of all swarms
+#ifdef MULTI_COMPONENT
+      real :: vol_0D
+#endif
       real                                             :: mdust       ! mass of the dust in the simulatated domain
       real, dimension(2)                               :: rand        ! random numbers
       real                                             :: Hg          ! pressure height scale of the gas
       integer                                          :: i
       real, parameter                                  :: s = 0.0     ! to initialize the particles density r slope (for MMSN s = -0.25)
       real                                             :: mswarm      ! initially all particles have the same mswarm + size
+#ifdef MULTI_COMPONENT
+      real                                             :: dz_0D, rmean, St0, a0
+      dz_0D = 0.00001*AU
+      rmean = 0.5*(minrad0+maxrad0)*AU
+      vol_0D = 2.*pi*rmean*(maxrad0-minrad0)*AU*dz_0D
+      a0 = 1. ! cm-sized initially
+      St0 = pi/2.*a0*matdenssi/sigmag(rmean, 0.)  ! Epstein regime
+      write(*,*) 'St0 and a0 are ', St0, a0
+      write(*,*) 'midplane dust-to-gas ratio is ', dtg/(sqrt(alpha_t/(alpha_t+St0)))
+      mdust = dtg/(sqrt(alpha_t/(alpha_t+St0))) * gasmass(minrad0*AU,maxrad0*AU,dz_0D, 0.0)
+#else
       ! total mass of dust =  dust to gas ratio x mass of the gas               
       mdust = dtg * gasmass(minrad0*AU,maxrad0*AU,0.0)
+#endif
+
       ! mass of one swarm
       mswarm = mdust / real(Ntot) ! this is for total
       ! monomer mass
@@ -52,21 +73,18 @@ module initproblem
       ! initializing the particles
       do i = 1, Ntot
          swrm(i)%idnr = i
+#ifdef MULTI_COMPONENT
+         swrm(i)%rdis = (minrad0 + (maxrad0-minrad0)*real(i-1)/real(Ntot-1))*AU
+         swrm(i)%fw = 0.
+         swrm(i)%w = swrm(i)%fw/(1.-swrm(i)%fw)
+         swrm(i)%rhoi = f_dens(swrm(i)%w)
+         swrm(i)%mass = 4. * third * pi * a0**3 * swrm(i)%rhoi
+         swrm(i)%mswarm = mswarm ! we track mswarm_silicates; this is valid because initially all water vapor
+         swrm(i)%npar = swrm(i)%mswarm * (1.+swrm(i)%w)/ swrm(i)%mass 
+         swrm(i)%zdis = (-0.5*dz_0D + dz_0D*real(i-1)/real(Ntot-1))
+#else
          call random_number(rand)
          swrm(i)%rdis = (((maxrad0*AU)**(s+1.) - (minrad0*AU)**(s+1.))*rand(1) + (minrad0*AU)**(s+1.))**(1./(s+1.))
-#ifdef MULTI_COMPONENT
-         swrm(i)%fw = 0.
-         swrm(i)%w = 0.
-         swrm(i)%rhoi = f_dens(swrm(i)%w)
-         swrm(i)%mass = 4. * third * pi * r0**3 * swrm(i)%rhoi
-         swrm(i)%mswarm = mswarm/(1.+swrm(i)%w) ! we track mswarm_silicates
-         swrm(i)%npar = swrm(i)%mswarm * (1.+swrm(i)%w)/ swrm(i)%mass 
-
-         ! 0D simulation; but do not set all particles at same r or z. Otherwise grid volume 0.
-         call random_number(rand)
-         swrm(i)%zdis = 0.001 *AU* sqrt(-2.*log(rand(1))) * cos(2.*pi*rand(2))
-#else
-
 #ifdef KERNEL_TEST
          swrm(i)%mass = 1.
          swrm(i)%mswarm = 10.e20
@@ -119,11 +137,11 @@ module initproblem
    end subroutine init_random_seed
 
 #ifdef MULTI_COMPONENT
-   real function f_dens(fw)
+   real function f_dens(w)
       implicit none
-      real, intent(in) :: fw
+      real, intent(in) :: w
 
-      f_dens = (1. + fw)/(fw/matdensw + 1./matdenssi)
+      f_dens = (1. + w)/(w/matdensw + 1./matdenssi)
 
       return
    end function 
