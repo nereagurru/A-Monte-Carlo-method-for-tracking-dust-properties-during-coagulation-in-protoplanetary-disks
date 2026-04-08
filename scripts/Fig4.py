d53a579269dbcd91021527017a8da0e4a97a50c6
Nerea Gurrutxaga <78503555+nereagurru@users.noreply.github.com>
2026-04-08 10:43:38 +0200

    Update Fig4.py
    Request by editor to make subplots in 1 row

--- scripts/Fig4.py
+++ scripts/Fig4.py
@@ -6,7 +6,7 @@ Created on Tue Feb 17 11:46:14 2026
 @author: gurrutxaga
 """
 
-# script to reproduce Figure 4 in the paper of global disk
+# script to reproduce figure 6 in the paper
 
 import numpy as np
 from Plotting import init_plot
@@ -18,13 +18,12 @@ from astropy import units as u
 import matplotlib as mpl
 
 
-
 time = 10e4 # years
 nmbins = 100
 rbins = 100
 
 au = u.au.to(u.cm)
-width=30.
+width=20.
 
 sigMax = 2.
 sigMin = -7.
@@ -32,6 +31,8 @@ levels = np.arange(sigMin, sigMax, 1)
 colors = plt.cm.magma_r(np.linspace(0, 1, len(levels)-1))
 colors[0] = [1, 1, 1, 1]
 cmap = mpl.colors.ListedColormap(colors)
+
+
 
 size_walls = np.logspace(np.log10(10**(-4)),
                      np.log10(20), nmbins+1)
@@ -44,18 +45,18 @@ rlogwalls = np.logspace(np.log10(1.),
 rlogcents = np.sqrt(rlogwalls[1:]*rlogwalls[:-1])
 drlog = rlogwalls[1:]-rlogwalls[:-1]
 
-path_list = ['../outputs/global_disk_mcdust/',
-             '../outputs/global_disk_X1/',
-             '../outputs/global_disk_X2/',
-             '../outputs/global_disk_X3/']
+path_list = ['/scratch/gurrutxaga/2DMC/mcdust/outputs/global_disk_mcdust/',
+             '/scratch/gurrutxaga/2DMC/mcdust/outputs/global_disk_X1/',
+             '/scratch/gurrutxaga/2DMC/mcdust/outputs/global_disk_X2/',
+             '/scratch/gurrutxaga/2DMC/mcdust/outputs/global_disk_X3/']
 
 
 
-fig,axx = plt.subplots(nrows=2, ncols=2, figsize=(width, 0.8*width), dpi=300,
+fig,axx = plt.subplots(nrows=1, ncols=4, figsize=(2*width, 0.4*width), dpi=300,
                      sharex=True, sharey=True)
+font = 40 # fonsize for title
 
-
-for i_plot, (path, ax) in enumerate(zip(path_list,axx.flatten())):
+for i_plot, (path, ax) in enumerate(zip(path_list,axx)):
     init_plot(ax)
 
     # read last snapshot
@@ -69,7 +70,7 @@ for i_plot, (path, ax) in enumerate(zip(path_list,axx.flatten())):
         rdis = np.array(swarmlist['cylindrical radius [AU]'])[0,:]
         mass = np.array(swarmlist['mass of a particle [g]'])[0,:]
 
-        # internal density of dust is 1g/cm**3
+        # density is 1g/cm**3
         grain_size = (0.75/np.pi*mass)**(1./3.)
         # Read compound dataset from swarms/swarmsout
         if i_plot == 0:
@@ -100,7 +101,11 @@ for i_plot, (path, ax) in enumerate(zip(path_list,axx.flatten())):
 
     pcf = ax.contourf(rlogcents / au, size_cents, np.log10(mdens1.T),
                       levels=levels, extend="both", cmap=cmap)
-
+    
+    # let more space between numbers in x
+    ax.tick_params(axis='x', pad=15)
+    ax.set_xticks([1, 10, 100])
+    ax.set_xticklabels(['1', '10', '100'])
     if i_plot==0:
         # Axes settings
         ax.set_xscale("log")
@@ -111,20 +116,19 @@ for i_plot, (path, ax) in enumerate(zip(path_list,axx.flatten())):
         ax.set_ylim(1e-4, 20)
 
         ax.tick_params(width=1., which='both')
-        ax.set_title('mcdust')
+        ax.set_title('mcdust', fontsize=font)
         
     elif i_plot==1:
-        ax.set_title('X=0.1')
+        ax.set_title('X=0.1', fontsize=font)
     elif i_plot==2:
-        ax.set_ylabel("dust size [cm]")
-        ax.set_title('X=0.01')
-        ax.set_xlabel("distance from star [au]")
+        ax.set_title('X=0.01', fontsize=font)
     else:
-        ax.set_title('X=0.001')
-        ax.set_xlabel("distance from star [au]")
+        ax.set_title('X=0.001', fontsize=font)
 
 cbar = fig.colorbar(pcf, ax=axx)
 cbar.set_label(r'$\log \left(\frac{\sigma_\mathrm{d}(a)}{g/cm^{2}}\right)$')#/ [g/cm²]
 
-fig.subplots_adjust(wspace=0.1, hspace=0.15, top=0.85, right=0.77)
+
+fig.text(0.45, -0.05, 'distance from star [au]', ha='center')
+fig.subplots_adjust(wspace=0.13, hspace=0.15, top=0.85, right=0.77)
 fig.savefig('mcdust.pdf', dpi=400, bbox_inches='tight', format='pdf')
